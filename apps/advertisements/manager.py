a44d270ef5216691955a6f9a3db8d39c02bf364e
Hryhorii_Struk <136825012+hryhoriistruk@users.noreply.github.com>
2024-08-31 12:09:22 +0300

    Update manager.py

--- apps/advertisements/manager.py
+++ apps/advertisements/manager.py
@@ -4,7 +4,7 @@ from rest_framework.generics import get_object_or_404
 from core.exceptions.car_exception import PremiumException
 
 
-class AdvertisementManager(Manager):i
+class AdvertisementManager(Manager):
     def check_user_is_prem(self, user):
         if user.account.is_premium:
             return True
