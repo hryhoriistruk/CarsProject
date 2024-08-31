6f8075d62a231d821f95fcc47e99038949819d95
Hryhorii_Struk <136825012+hryhoriistruk@users.noreply.github.com>
2024-08-31 12:08:02 +0300

    Update 0001_initial.py

--- apps/advertisements/migrations/0001_initial.py
+++ apps/advertisements/migrations/0001_initial.py
@@ -4,7 +4,7 @@ import django.core.validators
 import django.db.models.deletion
 from django.conf import settings
 from django.db import migrations, models
-m
+
 
 class Migration(migrations.Migration):
     initial = True
