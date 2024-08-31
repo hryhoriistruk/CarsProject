c5e69461412f228165ddaa76154f251968bab98c
Hryhorii_Struk <136825012+hryhoriistruk@users.noreply.github.com>
2024-08-31 12:11:56 +0300

    Update views.py

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,4 +1,4 @@
-from rest_framework import exceptions
+from rest_framework import exceptions.
 from rest_framework.fields import Field
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
